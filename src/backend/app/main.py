651a07a4b68521f65afa8b2a53556201ad902437
rut31337 <rut31337@gmail.com>
2026-06-08 15:18:23 -0500

    feat: add timestamps to backend log format

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, format="%(levelname)s %(name)s: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s: %(message)s", datefmt="%H:%M:%S")
 
 from contextlib import asynccontextmanager
 
