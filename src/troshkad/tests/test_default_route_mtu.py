fd9245f5e15c1a09d12e027b17731bb443ac9022
rut31337 <rut31337@gmail.com>
2026-09-11 19:40:39 -0500

    style(mtu): black-format troshkad default-route MTU test

--- src/troshkad/tests/test_default_route_mtu.py
+++ src/troshkad/tests/test_default_route_mtu.py
@@ -2,11 +2,13 @@
 from unittest.mock import patch
 import troshkad
 
+
 def test_default_route_mtu_parses_ip_output():
     route = "default via 10.0.0.1 dev enp1s0 proto dhcp\n"
     link = "2: enp1s0: <> mtu 8900 qdisc ...\n"
     with patch("troshkad.subprocess.check_output", side_effect=[route, link]):
         assert troshkad._default_route_mtu() == 8900
+
 
 def test_default_route_mtu_returns_none_on_error():
     with patch("troshkad.subprocess.check_output", side_effect=Exception("boom")):
